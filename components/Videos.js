477f33c963ef51ee13bf298070882c1a14bd0956
Mike <mvg210@gmail.com>
2016-03-26 17:44:25 -0700

    Got the search function working

--- components/Videos.js
+++ components/Videos.js
@@ -6,14 +6,37 @@ class Videos extends Component {
       this.props.getVideos()
       console.log("getting videos");
   }
+  constructor(props) {
+      super(props);
+      this.state = {
+        searchString: ''
+      }
+  }
+  handleChange(e){
+      this.setState({searchString:e.target.value});
+  }
   render() {
     if(this.props.videos == undefined){
       return <p>Loading....</p>
     }
+    var videos = this.props.videos,
+            searchString = this.state.searchString.trim().toLowerCase();
+
+
+        if(searchString.length > 0){
+
+            // We are searching. Filter the results.
+
+            videos = videos.filter(function(l){
+                return l.title.toLowerCase().match( searchString );
+            });
+
+        }
     return (
       <div>
+        <input type="text" value={this.state.searchString} onChange={this.handleChange.bind(this)} placeholder="Type here" />
         <ul>
-          {this.props.videos.map(function(result) {
+          {videos.map(function(result) {
             return <li key={result.position}>{result.title}</li>;
           })}
         </ul>
